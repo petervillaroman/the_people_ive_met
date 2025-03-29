a29c0487b7e92e7e3c7c4f82cff8a01aeb3ab20e
Karell Peter Villaroman <petervillaroman@gmail.com>
2025-03-29 04:07:25 -0700

    Revert "basic crud"
    This reverts commit bbdd2bc771686bf652a847da9f846fb3460f5f7a.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,4 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,18 +8,7 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">People I've Met</h1>
-      
-      <Link 
-        to="/addNew"
-        className="bg-black text-white px-4 py-2 rounded inline-block hover:bg-gray-800"
-      >
-        Add New Person
-      </Link>
-      
-      {/* Rest of your index page content */}
-    </div>
-  );
+  return (<>
+  HELLO!
+  </>) 
 }
